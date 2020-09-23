7d0e1664a7de5dd66fcc0ed7203f889b729ad92f
serdarcw <serkangumus06@gmail.com>
2020-09-23 18:11:35 +0300

    last changes

--- python/hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
+++ python/hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'database-3.cuxdz3m00nkr.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'database-1.cbanmzptkrzf.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = '123456789'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
